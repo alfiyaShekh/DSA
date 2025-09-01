93ad58e109ef941a2e12a6eecf4cbeeac94dba17
Alfiya Shekh <alfiyashekh521@gmail.com>
2025-09-01 15:41:59 +0530

    Data types

---
+++ cpp/datatype.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+//#include<iostream>
+using namespace std;
+int main(){
+    //int
+    int x=10;
+    cout<<"Integer type: "<<x<<endl;
+
+    //long 
+    long y=15;
+    cout<<"long type: "<<y<<endl;
+
+    //float
+    float z=9.6;
+    float a=2;
+    cout<<"Float type: "<<x<<endl;
+    cout<<"Float type: "<<a<<endl;
+
+    //double
+    double b=8.7;
+    cout<<"Double: "<<b<<endl;
+   
+    //string
+    string s1;
+    string s2;
+    cin>>s1>>s2;
+    cout<<s1<<" "<<s2;
+
+    //char
+    char ch='a';
+    cout<<ch;
+
+    return 0;
+}

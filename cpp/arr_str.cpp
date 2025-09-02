42e97e17cac4cb538db0a77e97f5fd143c1ac6cb
Alfiya Shekh <alfiyashekh521@gmail.com>
2025-09-02 22:12:23 +0530

    Arrray,string

---
+++ cpp/arr_str.cpp
@@ -0,0 +1,29 @@
+// #include<iostream>
+#include<bits/stdc++.h> 
+using namespace std;
+int main(){
+    // 1D array
+    // int arr[5];
+    // cin>>arr[0]>>arr[1]>>arr[2]>>arr[3]>>arr[4];
+    // cout<<arr[0]<<endl;
+    
+    // arr[3] +=3;
+    // cout<<arr[3];
+
+// 2D array
+   int arr1[2][5]; // like 3 rows amd 5 columns
+    arr1[1][3]=17;
+   // cout<<arr1[1][3];
+
+   
+
+
+    // strings
+
+    string str="alfiya";
+    cout<<str[1]<<endl;
+    int len=str.size();
+    cout<<len;
+
+     return 0;
+}

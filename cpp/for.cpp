200cac08148086a3d60545ae17c96f9c5a5bec03
Alfiya Shekh <alfiyashekh521@gmail.com>
2025-09-02 22:25:16 +0530

    for,while,do-while

---
+++ cpp/for.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    // ** for loop **
+    for(int i=0;i<=10;i++){
+       // cout<<"Alfiya"<<endl;
+    } 
+
+    // ** while loop **
+    int i=1;
+    while(i<=10){
+        //cout<<"hello Alfiya !!"<<endl;
+        i++;
+    }
+
+    // ** do-while **
+    int j=1;
+    do{
+        cout<<"DSA"<<endl;
+        j++;
+    }while(j<=10);
+
+    return 0;
+}

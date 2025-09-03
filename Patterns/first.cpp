8c78b4590b6df17f6795af24e2c572dec94a5732
Alfiya Shekh <alfiyashekh521@gmail.com>
2025-09-03 20:13:28 +0530

    First pattern

--- Patterns/first.cpp
+++ Patterns/first.cpp
@@ -12,12 +12,27 @@ using namespace std;
 // 3) whatever u are printing print them inside the inner for loop
 // 4)Observe symmetry
 
+
+
+void print(int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
 int main(){
 
-    for(int i=0;i<4;i++){
-        for(int j=0;j<4;j++){
-            cout<<"*";
-        }
-         cout<<endl;
-    }
+    // for(int i=0;i<4;i++){
+    //     for(int j=0;j<4;j++){
+    //         cout<<"*";
+    //     }
+    //      cout<<endl;
+    // }
+   
+    int row;
+    cin>>row;
+    print(row);
+
 }

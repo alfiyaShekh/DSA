77078ffcfd16c1b820b8557d4bf69f1d2b450f1c
Alfiya Shekh <alfiyashekh521@gmail.com>
2025-09-05 14:16:39 +0530

    Thirteen pattern

---
+++ Patterns/thirteen.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+/*
+1
+2 3
+4 5 6
+7 8 9 10
+11 12 13 14 15
+
+*/
+
+int main(){
+    int start=1;
+    for(int i=0;i<5;i++){
+        
+        
+        for(int j=0;j<=i;j++){
+            cout<<start<<" ";
+            start++;
+        }
+        cout<<endl;
+    }
+}

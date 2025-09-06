faf629d653fcd19a9022c0ef1bd5b377a76ed459
Alfiya Shekh <alfiyashekh521@gmail.com>
2025-09-06 22:12:45 +0530

    Prime Number

---
+++ Basic Maths/5_prime.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num;
+    cout<<"Enter number: ";
+    cin>>num;
+    if(num<=1){
+        cout<<"Number is not prime";
+        return 0;
+    }
+   
+        for(int i=2;i<=num/2;i++){
+            if(num%i==0){
+                cout<<"Number is not prime";
+                return 0;         
+               }
+        }
+    
+    cout<<"Number is prime";
+
+}
